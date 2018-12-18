5bd64a4d523ee6366f9c5a2e4f1f8cc704d04c7c
Ian <ifkb99@gmail.com>
2018-12-17 23:34:23 -0500

    file io completed. no flags

--- huffman.cpp
+++ huffman.cpp
@@ -17,18 +17,30 @@
 	Try buffered reading to use less memory? (read in ez cachable blocks)
 */
 
-vector<std::string> read_file(std::string filename) {
+// std::vector<std::string> read_file(std::string filename) {
 
-}
+// }
 
 //returns chars and percentage of appearance as double
 std::vector< std::pair<char, double> > scan(std::string filename) {
 	//open file and start reading
+	std::vector< std::pair<char, double> > percentages;
 
-
+	return percentages;
 }
 
-void encode(std::string filename) {
+/*
+
+	Encode and decode return vector<string> for two reasons:
+
+	* decode will return the huffman tree as well, 
+		(do i actually want to do that?)
+
+	* Eventually the vector will contain one block of data apiece for ez writin
+
+*/
+
+std::vector<std::string> encode(std::ifstream& filestream) {
 	//scan file to get char values
 
 	//make huffman tree
@@ -38,9 +50,18 @@ void encode(std::string filename) {
 	//write huffman tree values to file, and EOT char (double newline?)
 
 	//write rest of file
+
+	//--------------right now this just returns a vector<string> of read file
+	std::vector<std::string> encoded;
+	std::string line;
+	while (getline(filestream, line)) {
+		encoded.push_back(line);
+	}
+	filestream.close();
+	return encoded;
 }
 
-void decode(std::string filename) {
+std::vector<std::string> decode(std::ifstream& filestream) {
 	//read tree, put that into separate memory from rest of file
 
 	//re-create tree using read character values
@@ -48,6 +69,13 @@ void decode(std::string filename) {
 	//make new file called filename.substr(filename.size()-5)
 
 	//decode compressed file and write to new file
+	std::vector<std::string> decoded;
+	std::string line;
+	while (getline(filestream, line)) {
+		decoded.push_back(line);
+	}
+	filestream.close();
+	return decoded;
 }
 
 int main(int argc, const char* argv[]) {
@@ -64,13 +92,26 @@ int main(int argc, const char* argv[]) {
 	if (dot != std::string::npos)
 	 	extension = filename.substr(dot);
 
+	//determine if file is to be encoded or decoded
+	std::ifstream input(filename);
+	std::ofstream output;
+	std::vector<std::string> file_data;
 	if (extension == ".huff") { //is already encoded
-		//decode file
-		decode(filename);
+		file_data = decode(input);
+		output.open(filename.substr(0, filename.size()-5)); //removes .huff
 	} else { //isn't encoded
-		//encode file
-		encode(filename);
+		file_data = encode(input);
+		output.open(filename + ".huff");
 	}
+
+	//deletes original file. is it better to overwrite original file?
+	std::remove(argv[1]);
+
+	//write data to file
+	for (const std::string& line : file_data) {
+		output << line;
+	}
+	output.close();
 
 	//calculate space saved
 
